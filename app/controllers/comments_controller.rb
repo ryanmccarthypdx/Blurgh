8bee42074e17958210c9e75f97b794595def3901
Ryan <ryanmccarthypdx@gmail.com>
2015-03-21 15:47:51 -0700

    Make the world's ugliest solution to an embedded form needed for both new and editing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
     @post = Post.find(params[:id])
+
   end
 
   def create
@@ -18,10 +19,27 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    render '_edit_form'
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment Updated!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Something went wrong!"
+      render :edit
+    end
+  end
 
 private
   def comment_params
-    params.require(:comment).permit(:text, :post_id)
+    params.require(:comment).permit(:text, :post_id, :id)
   end
 
 end
