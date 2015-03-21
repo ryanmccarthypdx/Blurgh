bc86828ec9284d440c6b41cd5deebb1ae1e38c26
Ryan <ryanmccarthypdx@gmail.com>
2015-03-21 15:50:24 -0700

    Add delete functionality to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -37,6 +37,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:text, :post_id, :id)
