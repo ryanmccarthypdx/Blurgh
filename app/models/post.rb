f29153a726146b516434710dc4a0193f25993877
Ryan <ryanmccarthypdx@gmail.com>
2015-03-29 12:51:40 -0700

    Add basic user model and activerecord associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
   validates :text, :presence => true
   # accepts_nested_attributes_for :comments
 end
---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+  has_many :posts
+  has_many :comments
+
+  attr_accessor :password
+  validates_confirmation_of :password
+  before_save :encrypt_password
+  validates_uniqueness_of :email
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+end
