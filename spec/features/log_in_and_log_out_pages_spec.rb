c0c565c812dbc2f3d71ed28ed008ecd6fdca52b3
Ryan <ryanmccarthypdx@gmail.com>
2015-03-29 14:54:20 -0700

    Add full logout capability and test coverage

--- spec/features/log_in_and_log_out_pages_spec.rb
+++ spec/features/log_in_and_log_out_pages_spec.rb
@@ -28,6 +28,33 @@ describe "the login and logout process" do
     expect(page).to have_content "Login successful!"
   end
 
+  it "doesn't display the 'Log out' or 'you are logged in' info if not logged in" do
+    FactoryGirl.create(:user)
+    visit root_path
+    expect(page).to have_no_content "You are logged in as:"
+    expect(page).to have_no_content "Log out"
+  end
+
+  it "displays username and log out option if you are logged in" do
+    FactoryGirl.create(:user)
+    visit root_path
+    fill_in "Name", :with => "Ryan"
+    fill_in "Password", :with => "test"
+    click_on "Log in"
+    expect(page).to have_content "You are logged in as: Ryan"
+    expect(page).to have_content "Log out"
+  end
+
+  it "logs out correctly" do
+    FactoryGirl.create(:user)
+    visit root_path
+    fill_in "Name", :with => "Ryan"
+    fill_in "Password", :with => "test"
+    click_on "Log in"
+    click_on "Log out"
+    expect(page).to have_no_content "Ryan"
+  end
+
 
 
 end
